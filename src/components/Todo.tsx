9d689faec9501b3e6b60539767be65dc12659908
Yuliia <julia.novak9@gmail@com>
2023-02-07 21:38:40 +0100

    separate Todo from Todos Comp

---
+++ src/components/Todo.tsx
@@ -0,0 +1,5 @@
+const ToDoItem: React.FC<{ text: string }> = (props) => {
+  return <li>{props.text}</li>
+}
+
+export default ToDoItem
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import Todo from "../models/todo"
+import ToDoItem from "./Todo"
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+      {props.items.map(item => <ToDoItem key={item.id} text={item.text} />)}
     </ul>
   )
 }
